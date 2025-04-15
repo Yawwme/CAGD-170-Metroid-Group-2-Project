104e1e700aeee033542710be0bed3951030c1d0e
Yawwme <jannkarlomoralesfigueroa@gmail.com>
2025-04-15 16:05:51 -0700

    bang bang
    bang bang she shot me

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
     public float jumpforce = 10;
+    public float killHeight = -5;
+    public float stunTimer = 0;
+    public GameObject projectilePrefab;
+    public float timeBetweenShots;
+    public float startDelay;
+
     public int coins = 0;
     public int health = 99; // Starting HP
-    public float killHeight = -5;
-    public float stunTimer = 5;
-
+    
+    
     private Vector3 respawnPoint;
     private Rigidbody rb;
 
@@ -37,6 +42,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Jump();
+        Shoot();
         if (transform.position.y < killHeight)
             LoseHealth();
     }
@@ -76,6 +82,37 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
         }
     }
+
+    /// <summary>
+    /// Allows Samus to shoot bullets when space is pressed
+    /// </summary> 
+    private void Shoot()
+    {
+        //Work on how many bullets you can fire and holding the fire button
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            InvokeRepeating("SpawnProjectile", startDelay, timeBetweenShots);
+
+
+        }
+
+        //If the bullets collide with anything then it gets destroyed
+
+    }
+
+    /// <summary>
+    /// jaja
+    /// </summary>
+    public void SpawnProjectile()
+    {
+        //Remember to change the GetComponent into the bullet script (once bullets get added)
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        if (projectile.GetComponent<Laser>())
+        {
+            projectile.GetComponent<Laser>().goingLeft = goingLeft;
+        }
+    }
+
 
     private bool OnGround()
     {
@@ -143,20 +180,10 @@ public class PlayerController : MonoBehaviour
         {
             HardLoseHealth();
         }
-        if (other.tag == "Laser")
-        {
-            StartCoroutine(Stun());
-        }
     }
    
 
-    IEnumerator Stun()
-    {
-        int currentPlayerSpeed = (int)speed;
-        speed = 0;
-        yield return new WaitForSeconds(stunTimer);
-        speed = currentPlayerSpeed;
-    }
+ 
     private IEnumerator InvincibilityCoroutine()
     {
         isInvincible = true;
